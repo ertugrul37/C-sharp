5c9e76b0a3cd8049aea92a244a280ac2370ccacb
ertugrul gunes <ertugrulgunes37@gmail.com>
2021-01-26 01:12:37 +0300

    diziler

---
+++ diziler/dizler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kisi = { "ali", "can" };
+            int[] can = { 1, 2, 3, 4, 5 };
+            char[] karakter = { 'a', 'b' };
+
+            Console.Write(kisi[1] + "-" + can[3] + "-" + karakter[0]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
