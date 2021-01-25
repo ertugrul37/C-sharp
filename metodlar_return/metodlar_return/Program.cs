453919b8ea4e65d5af296907304aed0601166767
ertugrul gunes <ertugrulgunes37@gmail.com>
2021-01-26 01:30:05 +0300

    methodlar_return

---
+++ metodlar_return/metodlar_return/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodlar_return
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oku = Program.degeroku();
+            // int can = sayıiste();               (sayı için böyle yazılır)
+            Console.WriteLine(oku);
+            Console.ReadLine();
+
+        }
+        private static string degeroku()
+        {
+            return Console.ReadLine();
+          //  return Convert.ToInt32(Console.ReadLine());    (sayı için böyle yazılır)
+        }
+    }
+}
